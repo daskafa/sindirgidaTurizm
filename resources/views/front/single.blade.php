b5d439cf8310023179790d23b5c00367fb394d78
daskafa <ydaskafa@gmail.com>
2020-08-24 17:34:06 +0300

    i dont dont now this error but fixeeeeed

--- resources/views/front/single.blade.php
+++ resources/views/front/single.blade.php
@@ -7,7 +7,7 @@
     <div class="col-md-8 mx-auto offset-md-4">
     <h3 style="margin-top:1em;">{{$article->title}}</h3>
     <p>{{$article->content}}</p>
-    <img class="homepageimg" src="{{$article->image}}">
+    <img class="homepageimg" src="{{'/' . $article->image}}">
     </div>
   </div>
 </div>
